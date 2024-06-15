263a3074e7394691dcad30073621ce44a8bf2c00
thicco-catto <cristobal.martin01@gmail.com>
2024-06-15 17:37:04 +0200

    chore: Added test log

--- src/app/api/survey/[surveyId]/question/route.ts
+++ src/app/api/survey/[surveyId]/question/route.ts
@@ -1,5 +1,6 @@
 import { BadRequest, Created, NotFound, Ok, RouteParams, Unauthorized } from "@/lib/routeHelper";
 import { QuestionCreationDTOFromJSON } from "@/models/dto/questionCreationDTO";
+import { VerifyToken } from "@/repository/auth";
 import { AddQuestion, GetAllQuestions } from "@/repository/questionRepository";
 import { AddQuestionToOrder } from "@/repository/surveyRepository";
 import { NextRequest } from "next/server";
@@ -26,6 +27,9 @@ export async function POST(request: NextRequest, { params }: RouteParams<Params>
         }
 
         const idToken = authHeader.split(' ')[1];
+        const decodedToken = await VerifyToken(idToken);
+
+        console.log(decodedToken);
 
         const json = await request.json();
 
